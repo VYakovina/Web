8eb6684a47f12500193f275f10cf0760958956fc
VYakovina <yakovina85@gmail.com>
2017-09-21 01:14:00 +0300

    .

--- src/main/java/Relational.java
+++ src/main/java/Relational.java
@@ -1,7 +1,4 @@
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Relational")
@@ -12,4 +9,31 @@ public class Relational {
     @Id
     @ManyToOne
     private Package packCustom;
+
+    @Column(name = "numberPhone")
+    private Integer numberPhome;
+
+    public Relational getRationalPack() {
+        return rationalPack;
+    }
+
+    public void setRationalPack(Relational rationalPack) {
+        this.rationalPack = rationalPack;
+    }
+
+    public Package getPackCustom() {
+        return packCustom;
+    }
+
+    public void setPackCustom(Package packCustom) {
+        this.packCustom = packCustom;
+    }
+
+    public Integer getNumberPhome() {
+        return numberPhome;
+    }
+
+    public void setNumberPhome(Integer numberPhome) {
+        this.numberPhome = numberPhome;
+    }
 }
