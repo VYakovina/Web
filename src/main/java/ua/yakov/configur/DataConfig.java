dcad425a9937a221f8cb492bc19e869303b113c9
VYakovina <yakovina85@gmail.com>
2017-11-03 00:03:22 +0200

    .

--- src/main/java/ua/yakov/configur/DataConfig.java
+++ src/main/java/ua/yakov/configur/DataConfig.java
@@ -1,19 +1,44 @@
 package ua.yakov.configur;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.annotation.Resource;
-import java.util.Properties;
+import javax.sql.DataSource;
 
+
+import java.util.Properties;
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:propertyFile.properties")
 public class DataConfig {
     @Resource
     private Environment env;
 
-private Properties getHibernateProperties(){
+@Bean
+public Properties getHibernateProperties(){
     Properties properties = new Properties();
-
-
+    properties.put("hibernate.dialect",env.getRequiredProperty("db.hibernate.dialect"));
+    properties.put("hibernate.show_sql",env.getRequiredProperty("db.hibernate.show_sql"));
+    properties.put("hibernate.hbm2ddl.auto",env.getRequiredProperty("db.hibernate.hbm2ddl.auto"));
+    properties.put("hibernate.connection.CharSet",env.getRequiredProperty("db.hibernate.connection.CharSet"));
+    properties.put("hibernate.connection.characterEncoding",env.getRequiredProperty("db.hibernate.connection.characterEncoding"));
+    properties.put("hibernate.connection.useUnicode",env.getRequiredProperty("db.hibernate.connection.useUnicode"));
     return properties;
+}
+ @Bean
+public DataSource dataSource (){
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(env.getRequiredProperty("db.driver_class"));
+    dataSource.setUrl(env.getRequiredProperty("db.url"));
+    dataSource.setUsername(env.getRequiredProperty("db.username"));
+    dataSource.setPassword(env.getRequiredProperty("db.password"));
+return dataSource;
+
 }
 
 }
